7f9620e460cf307edc4b3c9aa15bc3db00dbc22f
Eric L. Bentley <11730553+ebntly@users.noreply.github.com>
2024-04-21 20:28:06 +0000

    docs: update module header

--- mod.ts
+++ mod.ts
@@ -1,9 +1,11 @@
 /**
+ * @copyright 2020-2024 integereleven. All rights reserved. MIT license.
+ * @file Public API exports for the module.
+ * 
  * The @kz/common-types module provides a collection of the most common types
  * used across kz packages, adapted for API consistency and case-coverage,
  * exhaustively tested, and documented thoroughly.
  *
- * @copyright 2020-2024 integereleven. All rights reserved. MIT license.
  * @module
  */
 
